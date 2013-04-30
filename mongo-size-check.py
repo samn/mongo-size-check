cdf6fe5ae866241e4f1825e62f29e52c902ecdde
Sam Neubardt <samneubardt@gmail.com>
2013-04-30 14:03:14 -0400

    Add max capacity & num days to compare as attributes
    on the riemann event.
    
    * Add real arg parsing
    * Refactor

--- mongo-size-check.py
+++ mongo-size-check.py
@@ -1,46 +1,61 @@
 import json
+from optparse import OptionParser
 import subprocess
 import sys
 
-JS_SENTINEL = "WWWEB$CALE"
+class MongoSizeCheck():
+    # The mongo command outputs stuff before showing the output of what it runs
+    # this is used to split that out so the rest can be parsed as json.
+    JS_SENTINEL = "WWWEB$CALE"
 
-JS_SIZES = """
-var out = [];
-db.getCollectionNames().forEach(function(collection) {
-    var collectionInfo = {name: collection};
-    var size = db.runCommand({collStats: collection})['storageSize'];
-    collectionInfo["size"] = size;
-    out.push(collectionInfo);
-})
-print("%s");
-printjson(out);
-""" % JS_SENTINEL
+    JS_SIZES = """
+    var out = [];
+    db.getCollectionNames().forEach(function(collection) {
+        var collectionInfo = {name: collection};
+        var size = db.runCommand({collStats: collection})['storageSize'];
+        collectionInfo["size"] = size;
+        out.push(collectionInfo);
+    })
+    print("%s");
+    printjson(out);
+    """ % JS_SENTINEL
 
-def strip_mongo_preamble(mongo_output):
-    return mongo_output.partition(JS_SENTINEL)[-1]
+    def __init__(self, options):
+        self.database = options.database
+        self.host = options.host
+        self.max_collection_size = options.max_collection_size 
+        self.compare_days = options.compare_days
 
-def get_collections_info(host, database):
-    output = subprocess.check_output(["mongo", database, "--host", host, "--eval", JS_SIZES])
-    return json.loads(strip_mongo_preamble(output))
+    def strip_mongo_preamble(self, mongo_output):
+        return mongo_output.partition(self.JS_SENTINEL)[-1]
 
-def construct_event(collection_info):
-    event = {}
-    event["service"] = "mongodb collection sizes"
-    event["state"] = "warn"
-    event["description"] = collection_info["name"]
-    event["metric"] = collection_info["size"]
-    return event
+    def get_collections_info(self):
+        output = subprocess.check_output(["mongo", self.database, "--host", self.host, "--eval", self.JS_SIZES])
+        return json.loads(self.strip_mongo_preamble(output))
 
-def report_collection_sizes(database, host):
-    events = [construct_event(e) for e in get_collections_info(host, database)]
-    print json.dumps(events)
+    def construct_event(self, collection_info):
+        event = {}
+        event["service"] = "mongodb collection sizes"
+        event["state"] = "warn"
+        event["description"] = collection_info["name"]
+        event["metric"] = collection_info["size"]
+        event["attributes"] = {}
+        event["attributes"]["compare_days"] = self.compare_days
+        event["attributes"]["max_collection_size"] = self.max_collection_size
+        return event
+
+    def report_collection_sizes(self):
+        events = [self.construct_event(e) for e in self.get_collections_info()]
+        print json.dumps(events)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print "Usage: %s database [host=localhost]" % sys.argv[0]
-        sys.exit(1)
-    database = sys.argv[1]
-    host = "localhost"
-    if len(sys.argv) > 2:
-        host = sys.argv[2]
-    report_collection_sizes(database, host)
+    parser = OptionParser()
+    parser.add_option("--database", help="Monitor the collections of this database")
+    parser.add_option("--host", default="localhost", help="The host of the database to monitor")
+    parser.add_option("--max-collection-size", default=256, help="The max allowed size of a collection in GB")
+    parser.add_option("--compare-days", default=7, help="The number of days to use to calculate a rate of growth")
+    options, args = parser.parse_args()
+    if options.database is None:
+        parser.print_help()
+        exit(-1)
+    MongoSizeCheck(options).report_collection_sizes()
